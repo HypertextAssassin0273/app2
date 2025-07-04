b824636bfc311aaaae88bdb5f9b4f8a5abd9b73d
Shahzaib Khan <k190273@nu.edu.pk>
2025-07-05 04:02:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>MY APP 2</h1><p>Welcome to App 2!</p><p> check out my other app too at <a href="https://google-custom-search.softwares.calyxtech.net"</a>! Bieee!</p><h2>UPDATE SUCCESSFUL!</p>"
+    return "<h1>MY APP 2</h1><p>Welcome to App 2!</p><p> check out my other app too at <a href=\"https://google-custom-search.softwares.calyxtech.net\">google-custom-search.softwares.calyxtech.net</a>! Bieee!</p><h2>UPDATE SUCCESSFUL!</h2>"
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
